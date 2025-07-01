cdd99e5ae992801f024b3b7e5f7fca4e0b455919
pedrobocatto <145362033+pedrobocatto@users.noreply.github.com>
2025-07-01 11:37:00 -0300

    01062025

---
+++ script.js
@@ -0,0 +1,36 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-peruntas");
+const caixaAlternativas = document.querySelector("caixa-alternativas");
+const caixaResultado = document.querySelector("caixa-resultado");
+const textoResultado = document.querySelector("texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: Assim que saiu da escola, ele recebeu um abraço da garota que gosta:
+        alternativas: [
+            "Isso é assustador!",
+            "Isso é maravilhoso",
+        ]
+    },
+    {
+        enunciado: Assim que saiu da escola, ele recebeu um abraço da garota que gosta:
+        alternativas: [
+            "Isso é assustador!",
+            "Isso é maravilhoso",
+        ]
+    },
+    {
+       enunciado: Assim que saiu da escola, ele recebeu um abraço da garota que gosta:
+        alternativas: [
+            "Isso é assustador!",
+            "Isso é maravilhoso",
+        ]
+    },
+    {
+        enunciado: Assim que saiu da escola, ele recebeu um abraço da garota que gosta:
+        alternativas: [
+            "Isso é assustador!",
+            "Isso é maravilhoso",
+        ]
+    },
+]
